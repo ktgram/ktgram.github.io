7718438b345916f87a3de7f65d4870570944d60a
vendelieu <vendelieu@gmail.com>
2025-01-11 06:41:31 +0300

    translate wflow

--- node_modules/i18n-ai-translate/src/interfaces/chats.ts
+++
@@ -1,7 +0,0 @@
-import type ChatInterface from "../chat_interface/chat_interface";
-
-export default interface Chats {
-    generateTranslationChat: ChatInterface;
-    verifyTranslationChat: ChatInterface;
-    verifyStylingChat: ChatInterface;
-}
